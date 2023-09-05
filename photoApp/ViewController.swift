6ec93bee332df2dc3a62eb6ab3bae552dd6d891c
Alihan <alihan0hoylu@gmail.com>
2023-09-05 14:40:23 +0300

    interface done

--- photoApp/ViewController.swift
+++ photoApp/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func signUpButton(_ sender: Any) {
+        performSegue(withIdentifier: "toFeed", sender: nil)
+    }
+    
 }
 
